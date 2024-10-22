cdb4e76654aa05f7eeac93ec898694f95db8bb1a
Angel <145053589+Angel170605@users.noreply.github.com>
2024-10-22 19:33:59 +0100

    Delete supertodo directory

--- supertodo/justdoit/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import ToDo
-
-
-@admin.register(ToDo)
-class ToDoAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'created_at', 'updated_at']
-    repopulated_fields = {'slug': ['title']}
--- supertodo/justdoit/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import ToDo
-
-
-class AddTaskForm(forms.ModelForm):
-    class Meta:
-        model = ToDo
-        fields = ('name', 'description')
