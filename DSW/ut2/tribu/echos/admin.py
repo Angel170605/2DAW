f387c0dddd13c61a1d438252114bb3123f59ee46
Angel <145053589+Angel170605@users.noreply.github.com>
2024-10-24 19:14:16 +0100

    Delete DSW/ut2/tribu directory

--- DSW/ut2/tribu/echos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Echo
-
-
-@admin.register(Echo)
-class EchoAdmin(admin.ModelAdmin):
-    list_display = ['content', 'created_at', 'updated_at', 'user']
