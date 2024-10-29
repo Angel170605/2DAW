cb10d38887080ddebdf0339dc2cff17d839c8ede
Angel <145053589+Angel170605@users.noreply.github.com>
2024-10-29 20:26:52 +0000

    Delete DSW/ut2/tribu2 directory

--- DSW/ut2/tribu2/shared/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth import get_user_model
-
-
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model = get_user_model()
-        fields = ['username', 'first_name', 'last_name', 'email', 'password']
-        widgets = dict(password=forms.PasswordInput)
