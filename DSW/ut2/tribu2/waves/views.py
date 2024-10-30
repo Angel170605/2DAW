0191864ad4efe1e77b64fe4a185b4a9c23b5c6df
Angel <145053589+Angel170605@users.noreply.github.com>
2024-10-30 20:19:00 +0000

    Delete DSW/ut2/tribu2 directory

--- DSW/ut2/tribu2/waves/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from waves.models import Wave
-
-
-def waves(request):
-    waves = Wave.objects.all()
-    return render(request, 'waves.html', {'waves': waves})
