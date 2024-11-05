d6f39a87de355a87e47e2347745ad6339c0f76c2
Angel <145053589+Angel170605@users.noreply.github.com>
2024-11-05 20:17:37 +0000

    Delete DSW/ut2/tribu2 directory

--- DSW/ut2/tribu2/waves/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Wave
-
-
-@admin.register(Wave)
-class WaveAdmin(admin.ModelAdmin):
-    list_display = ['content', 'created_at', 'updated_at', 'user', 'echo']
--- DSW/ut2/tribu2/waves/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Wave
-
-
-class AddWaveForm(forms.ModelForm):
-    class Meta:
-        model = Wave
-        fields = ('content',)
